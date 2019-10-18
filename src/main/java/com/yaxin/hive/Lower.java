96f9292be4c2c4423a56b1e896ef3da488560187
hgf-anu <956246882@qq.com>
2019-10-18 10:36:53 +0800

    exec Hive UDF

--- src/main/java/com/yaxin/hive/Lower.java
+++ src/main/java/com/yaxin/hive/Lower.java
@@ -1,7 +1,18 @@
 package com.yaxin.hive;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.hadoop.hive.ql.exec.UDF;
 
 public class Lower extends UDF {
 
+    /**
+     * evaluate不是继承也不是实现接口,写该方法的时候一定不要写错evaluate的拼写
+     */
+    public String evaluate(String original){
+        if(StringUtils.isBlank(original)){
+            return null;
+        }
+        return original.toLowerCase();
+
+    }
 }
